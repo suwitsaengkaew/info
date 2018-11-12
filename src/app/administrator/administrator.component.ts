541efed3f625fd6c3481acafc9dc0cbb471fd360
Suwit Saengkaew <suwit.saengkaew@gmail.com>
2018-11-12 17:00:44 +0700

    20181112

--- src/app/administrator/administrator.component.ts
+++ src/app/administrator/administrator.component.ts
@@ -19,26 +19,36 @@ export class AdministratorComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSelectRegisterItems(key: number) {
+  onSelectRegisterItems(key: any) {
+    
     switch (key) {
       case 1:
-        this.prtype = !this.prtype;
+        console.log(key);
         break;
-      case 2:
-        this.prplant = !this.prplant;
-        break;
-      case 3:
-        this.prrequestby = !this.prrequestby;
-        break;
-      case 4:
-        this.prsuppliers = !this.prsuppliers;
-        break;
-      case 5:
-        this.prunit = !this.prunit;
-        break;
-      case 6:
-        this.prcurrency = !this.prcurrency;
+      default:
         break;
     }
+    // console.log(key);
+    // switch (key) {
+    //   case 1:
+    //     console.log(key);
+    //     this.prtype = !this.prtype;
+    //     break;
+      // case 2:
+      //   this.prplant = !this.prplant;
+      //   break;
+      // case 3:
+      //   this.prrequestby = !this.prrequestby;
+      //   break;
+      // case 4:
+      //   this.prsuppliers = !this.prsuppliers;
+      //   break;
+      // case 5:
+      //   this.prunit = !this.prunit;
+      //   break;
+      // case 6:
+      //   this.prcurrency = !this.prcurrency;
+      //   break;
+    // }
   }
 }
